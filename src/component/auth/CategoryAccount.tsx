f98deb294bbcc69978997777dc0a4a9ddfa2beb0
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-12 15:28:18 +0200

    Updated auth

--- src/component/auth/CategoryAccount.tsx
+++ src/component/auth/CategoryAccount.tsx
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Pressable, ScrollView, Text, View } from "react-native";
 
 import Icon from "react-native-vector-icons/Ionicons";
 import Input from "../Input";
 
+import { getAllCategory } from "@/src/service/request/get";
+import { postCategoryQuery } from "@/src/service/request/post";
+import { CategoriesInterface } from "@/src/service/type/apiType/categoryType";
+
 import { CategoryAccountStyle } from "@/src/style/auth/account";
+import LottieView from "lottie-react-native";
 
 export default function CategoryAccount() {
+  const [categoriesData, setCategoriesData] = useState<
+    CategoriesInterface[] | undefined
+  >();
   const [category, setcategory] = useState("");
+
+  useEffect(() => {
+    postCategoryQuery(setCategoriesData, category);
+    if (category.length <= 0) {
+      getAllCategory(setCategoriesData);
+    }
+  }, [category]);
   return (
     <>
       <Text style={CategoryAccountStyle.title}>
@@ -32,99 +47,32 @@ export default function CategoryAccount() {
       </View>
       <Text style={CategoryAccountStyle.text}>Toutes les catégories</Text>
       <ScrollView style={CategoryAccountStyle.categories}>
-        <Pressable style={CategoryAccountStyle.categories_element}>
-          <Text style={CategoryAccountStyle.categories_element_title}>
-            Logement
-          </Text>
-          <Text style={CategoryAccountStyle.categories_element_text}>
-            Loyer, hypothèque, charges...
-          </Text>
-          <Icon
-            name="home-outline"
-            size={20}
-            style={CategoryAccountStyle.categories_element_icon}
+        {categoriesData?.map((category) => (
+          <Pressable
+            key={category.id}
+            style={CategoryAccountStyle.categories_element}
+          >
+            <Text style={CategoryAccountStyle.categories_element_title}>
+              {category.name}
+            </Text>
+            <Text style={CategoryAccountStyle.categories_element_text}>
+              {category.description}
+            </Text>
+            <Icon
+              name={category.icon}
+              size={20}
+              style={CategoryAccountStyle.categories_element_icon}
+            />
+          </Pressable>
+        ))}
+        {category.length > 0 && categoriesData?.length === 0 && (
+          <LottieView
+            source={require("../../../assets/lottie/notFound.json")}
+            autoPlay
+            loop
+            style={CategoryAccountStyle.categories_element_lottie}
           />
-        </Pressable>
-        {/* SUPPPP */}
-        <Pressable style={CategoryAccountStyle.categories_element}>
-          <Text style={CategoryAccountStyle.categories_element_title}>
-            Logement
-          </Text>
-          <Text style={CategoryAccountStyle.categories_element_text}>
-            Loyer, hypothèque, charges...
-          </Text>
-          <Icon
-            name="home-outline"
-            size={20}
-            style={CategoryAccountStyle.categories_element_icon}
-          />
-        </Pressable>
-        <Pressable style={CategoryAccountStyle.categories_element}>
-          <Text style={CategoryAccountStyle.categories_element_title}>
-            Logement
-          </Text>
-          <Text style={CategoryAccountStyle.categories_element_text}>
-            Loyer, hypothèque, charges...
-          </Text>
-          <Icon
-            name="home-outline"
-            size={20}
-            style={CategoryAccountStyle.categories_element_icon}
-          />
-        </Pressable>
-        <Pressable style={CategoryAccountStyle.categories_element}>
-          <Text style={CategoryAccountStyle.categories_element_title}>
-            Logement
-          </Text>
-          <Text style={CategoryAccountStyle.categories_element_text}>
-            Loyer, hypothèque, charges...
-          </Text>
-          <Icon
-            name="home-outline"
-            size={20}
-            style={CategoryAccountStyle.categories_element_icon}
-          />
-        </Pressable>
-        <Pressable style={CategoryAccountStyle.categories_element}>
-          <Text style={CategoryAccountStyle.categories_element_title}>
-            Logement
-          </Text>
-          <Text style={CategoryAccountStyle.categories_element_text}>
-            Loyer, hypothèque, charges...
-          </Text>
-          <Icon
-            name="home-outline"
-            size={20}
-            style={CategoryAccountStyle.categories_element_icon}
-          />
-        </Pressable>
-        <Pressable style={CategoryAccountStyle.categories_element}>
-          <Text style={CategoryAccountStyle.categories_element_title}>
-            Logement
-          </Text>
-          <Text style={CategoryAccountStyle.categories_element_text}>
-            Loyer, hypothèque, charges...
-          </Text>
-          <Icon
-            name="home-outline"
-            size={20}
-            style={CategoryAccountStyle.categories_element_icon}
-          />
-        </Pressable>
-        <Pressable style={CategoryAccountStyle.categories_element}>
-          <Text style={CategoryAccountStyle.categories_element_title}>
-            Logement
-          </Text>
-          <Text style={CategoryAccountStyle.categories_element_text}>
-            Loyer, hypothèque, charges...
-          </Text>
-          <Icon
-            name="home-outline"
-            size={20}
-            style={CategoryAccountStyle.categories_element_icon}
-          />
-        </Pressable>
-        {/* SUPPPPP */}
+        )}
       </ScrollView>
     </>
   );
