64037577fb884f324a6be3c8b52ee23e810e8aaf
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-16 02:57:20 +0200

    Request for user category

---
+++ src/service/type/apiType/userCatJoinCat.ts
@@ -0,0 +1,11 @@
+export interface CategoriesJoinInterface {
+  user_id: number;
+  category_id: number;
+  cap: number;
+  name: string;
+  description: string;
+  icon: string;
+  color: string;
+  colormid: string;
+  colorbg: string;
+}
