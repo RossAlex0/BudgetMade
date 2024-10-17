8a093bdfd7a7e78b3eb4e69f4cde03c52e570db1
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-17 11:54:09 +0200

    Added css & type for modal

---
+++ src/service/type/tabsType/modal.ts
@@ -0,0 +1,9 @@
+export interface PasswordModalInterface {
+  passwordIsSelect: boolean;
+  setPasswordIsSelect: (state: boolean) => void;
+}
+
+export interface ProfilModaleInterface {
+  profilIsSelect: boolean;
+  setProfilIsSelect: (state: boolean) => void;
+}
