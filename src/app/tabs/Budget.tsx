e5a753dd1ed8b347a5fcff7ec3bb55f9770f3d93
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-21 00:32:05 +0200

    Budget app

--- src/app/tabs/Budget.tsx
+++ src/app/tabs/Budget.tsx
@@ -1,64 +1,34 @@
-import { Text, View, StyleSheet } from "react-native";
-import React from "react";
-import PieChart from "react-native-pie-chart";
+import { Text, View, Pressable, ScrollView } from "react-native";
+import React, { useState } from "react";
+
+import BudgetDetails from "@/src/component/tabs/BudgetDetails";
+
+import { BudgetStyle } from "@/src/style/tabs/budget";
 
 export default function Budget() {
-  const salary = 10000;
-  const expenses = [1500, 2000, 2500, 500];
-  const totalDonut = salary - expenses.reduce((acc, sum) => acc + sum, 0);
-  let series = [];
-  let sliceColor = [];
-  if (salary < expenses.reduce((acc, sum) => acc + sum, 0)) {
-    series = [1];
-    sliceColor = ["red"];
-  } else {
-    series = [...expenses, totalDonut + salary];
-    sliceColor = ["#000000", "#ffb300", "#ff9100", "#ff6c00", "gray"];
-  }
-
+  const [filterTabs, setFilterTabs] = useState("details");
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Doughnut Chart</Text>
-      <View style={styles.chartContainer}>
-        <PieChart
-          widthAndHeight={250}
-          series={series}
-          sliceColor={sliceColor}
-          coverRadius={0.85}
-          coverFill={"transparent"}
-          style={styles.halfRing}
-        />
-        <Text style={styles.text}>{salary} €</Text>
+    <View style={BudgetStyle.budget}>
+      <View style={BudgetStyle.budget_header}>
+        <Text style={BudgetStyle.budget_header_text}>Portefeuille</Text>
+        <View style={BudgetStyle.budget_header_nav}>
+          <Pressable
+            style={BudgetStyle.nav_pressable}
+            onPress={() => setFilterTabs("details")}
+          >
+            <Text style={BudgetStyle.nav_pressable_text}>Détails</Text>
+          </Pressable>
+          <Pressable
+            style={BudgetStyle.nav_pressable}
+            onPress={() => setFilterTabs("category")}
+          >
+            <Text style={BudgetStyle.nav_pressable_text}>Catégorie</Text>
+          </Pressable>
+        </View>
       </View>
+      <ScrollView style={BudgetStyle.body}>
+        {filterTabs === "details" ? <BudgetDetails /> : ""}
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    paddingTop: 80,
-  },
-  title: {
-    fontSize: 24,
-    margin: 10,
-  },
-  chartContainer: {
-    width: 250,
-    height: 125,
-    overflow: "hidden",
-    display: "flex",
-    alignItems: "center",
-  },
-  halfRing: {
-    transform: [{ rotate: "-90deg" }],
-    borderRadius: 240,
-  },
-  text: {
-    fontFamily: "Regular",
-    fontSize: 24,
-    position: "absolute",
-    top: 84,
-  },
-});
